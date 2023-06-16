486b01dd0b7bd9b7b5374b1a60806151990f3c0c
basil-collette <basil.collette@laplateforme.io>
2023-06-16 13:24:14 +0200

    graph add

---
+++ ScriptableObject/EventNodeSO.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/EventNodeSO")]
+public class EventNodeSO : ScriptableObject
+{
+    public UnityEvent Event;
+
+    public void Invoke()
+    {
+        Event.Invoke();
+    }
+
+}
